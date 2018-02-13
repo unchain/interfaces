c516891c8659d3a273c911d0d22982d540be67fa
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-02-13 14:31:49 +0100

    introducing a TaggedMessage

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -8,14 +8,25 @@ import (
 	"github.com/unchainio/interfaces/logger"
 )
 
+type TaggedMessage struct {
+	Tag uint64
+	*Message
+}
+
 type Message struct {
-	Tag        uint64
 	Body       []byte
 	Attributes map[string]bool
 }
 
 type MessageOpts struct {
-	Tag uint64
+	tag uint64
+}
+
+func NewMessage(body []byte) *Message {
+	return &Message{
+		Body:       body,
+		Attributes: make(map[string]bool),
+	}
 }
 
 var defaultOpts = &MessageOpts{}
@@ -23,21 +34,20 @@ var defaultOpts = &MessageOpts{}
 type MessageOptsFunc func(opt *MessageOpts)
 
 // NewMessage constructs a new message with a random tag unless a custom one is specified via WithTag(tag uint64)
-func NewMessage(body []byte, optFuncs ...MessageOptsFunc) *Message {
+func NewTaggedMessage(body []byte, optFuncs ...MessageOptsFunc) *TaggedMessage {
 	opts := defaultOpts
 
 	for _, optFunc := range optFuncs {
 		optFunc(opts)
 	}
 
-	if opts.Tag == 0 {
-		opts.Tag = randomTag()
+	if opts.tag == 0 {
+		opts.tag = randomTag()
 	}
 
-	return &Message{
-		Tag:        opts.Tag,
-		Body:       body,
-		Attributes: make(map[string]bool),
+	return &TaggedMessage{
+		Tag:     opts.tag,
+		Message: NewMessage(body),
 	}
 }
 
@@ -47,20 +57,20 @@ func randomTag() uint64 {
 
 func WithTag(tag uint64) MessageOptsFunc {
 	return func(opts *MessageOpts) {
-		opts.Tag = tag
+		opts.tag = tag
 	}
 }
 
-type EndpointPlugin interface {
+type Endpoint interface {
 	Init(config []byte, log logger.Logger) (err error)
 	Send(message *Message) (response *Message, err error)
-	Receive() (message *Message, err error)
+	Receive() (message *TaggedMessage, err error)
 	Ack(tag uint64, response *Message) error
 	Nack(tag uint64) error
 	Close() error
 }
 
-type ActionPlugin interface {
+type Action interface {
 	Init(config []byte, log logger.Logger) (err error)
-	Invoke(message *Message) (result *Message, err error)
+	Invoke(message *Message) (err error)
 }
