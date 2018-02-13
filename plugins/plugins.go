e51aeadb7d3e8e64020bc471d19437cde161d831
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-02-13 12:45:14 +0100

    messages get a random tag by default

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -1,11 +1,54 @@
 package plugins
 
-import "github.com/unchainio/interfaces/logger"
+import (
+	"math/rand"
+
+	"time"
+
+	"github.com/unchainio/interfaces/logger"
+)
 
 type Message struct {
 	Tag        uint64
 	Body       []byte
 	Attributes map[string]bool
+}
+
+type MessageOpts struct {
+	Tag uint64
+}
+
+var defaultOpts = &MessageOpts{}
+
+type MessageOptsFunc func(opt *MessageOpts)
+
+// NewMessage constructs a new message with a random tag unless a custom one is specified via WithTag(tag uint64)
+func NewMessage(body []byte, optFuncs ...MessageOptsFunc) *Message {
+	opts := defaultOpts
+
+	for _, optFunc := range optFuncs {
+		optFunc(opts)
+	}
+
+	if opts.Tag == 0 {
+		opts.Tag = randomTag()
+	}
+
+	return &Message{
+		Tag:        opts.Tag,
+		Body:       body,
+		Attributes: make(map[string]bool),
+	}
+}
+
+func randomTag() uint64 {
+	return rand.New(rand.NewSource(time.Now().UnixNano())).Uint64()
+}
+
+func WithTag(tag uint64) MessageOptsFunc {
+	return func(opts *MessageOpts) {
+		opts.Tag = tag
+	}
 }
 
 type EndpointPlugin interface {
