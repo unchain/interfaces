00b7e404d9a3f00f4f614b555fb040876b6d7621
Henk van Ramshorst <henk@unchain.io>
2019-05-23 16:14:50 +0200

    add respond to interface and remove ack and nack

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -16,14 +16,11 @@ type Trigger interface {
 	// Trigger: must block until a new message is received
 	Trigger() (tag string, message map[string]interface{}, err error)
 
-	// Ack is called by the adapter base after the message (with tag `tag`), which was initially received
-	// by this input endpoint, has been successfully passed through the actions in the pipeline, sent
-	// over the output endpoint, and a response has been returned from the output endpoint and passed
-	// through the actions in the response pipeline.
-	Ack(tag string, response map[string]interface{}) error
-
-	// Nack is called by the adapter base if anything goes wrong while processing the message with tag `tag`
-	Nack(tag string, err error) error
+	// Respond: is called by the adapter base after the message (with tag `tag`), which was initially received
+	// by the Trigger, has passed through the actions in the pipeline. Respond is used to send a response back to
+	// the caller of the Trigger. In case of an error during invocation of an action component, the adapter base will
+	// discontinue invocation of other actions in the pipeline and call Respond.
+	Respond(tag string, response map[string]interface{}, err error) error
 
 	Close() error
 }
